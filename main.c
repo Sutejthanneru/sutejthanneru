6eb7908272c93120f086a71603f075034da532e6
Sutejthanneru <sutejtanneru41@gmail.com>
2024-10-17 15:28:25 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+<img src="https://github.com/PSVRAMANA40248/PSVRAMANA40248/blob/main/Screenshot%202024-10-14%20004348.png" alt="logo" width="1000" height="300"/>
+<h1 align="center">Hi 👋, I'm P.SAI VENKAT RAMANA</h1>
+<h2 align="center">A passionate Electronics Engineering And Bachelor of Business Administration (Online) from India</h2>
+<img align="right" alt="coding" width="400" src="https://user-images.githubusercontent.com/55389276/140866485-8fb1c876-9a8f-4d6a-98dc-08c4981eaf70.gif">
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/>  <a href="https://www.python.org/" target="_blank" rel="noreferrer">
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<h3 align="left">My Certificates:</h3>
+<p align="left">
+  <img src="https://github.com/PSVRAMANA40248/PSVRAMANA40248/blob/main/CERTIFICATE%20OF%20PYTHON.png" alt="My Certificate" width="50" />
+  <img src="https://github.com/PSVRAMANA40248/PSVRAMANA40248/blob/main/PROBLEM%20SOLVING(BASICS).png" alt="My Certificate" width="50" />
+  <img src="https://github.com/PSVRAMANA40248/PSVRAMANA40248/blob/main/Mat_Lab.png" alt="My Certificate" width="50" />
+</p>
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+  <a href="https://www.linkedin.com/in/pusarla-sai-venkat-ramana-a44931300/" target="blank">
+    <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="Pusarla Sai Venkat Ramana" height="30" width="40" />
+  </a>
+  <a href="https://instagram.com/pusarla_ramana" target="blank">
+    <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="Pusarla Ramana" height="30" width="40" />
+  </a>
+
+</p>
